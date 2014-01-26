00fdb14cadc69e52da26bbfced2ac1947fda7b48
mightymouse07 <carlos.cook07@gmail.com>
2014-01-26 10:47:06 -0800

    Using overlays.

--- src/com/example/barcodescanningapp/MainActivity.java
+++ src/com/example/barcodescanningapp/MainActivity.java
@@ -2,78 +2,152 @@ package com.example.barcodescanningapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-/**
- * This is demo code to accompany the Mobiletuts+ tutorial:
- * - Using Barcode Scanning in Android Apps
- * 
- * Sue Smith
- * May 2013
- *
- */
-public class MainActivity extends Activity implements OnClickListener {
 
-	//UI instance variables
-	private Button scanBtn;
+/**
+ * This is demo code to accompany the Mobiletuts+ tutorial: - Using Barcode
+ * Scanning in Android Apps
+ * 
+ * Sue Smith May 2013
+ * 
+ */
+public class MainActivity extends Activity {
+
+	// UI instance variables
 	private TextView formatTxt, contentTxt;
+	private ImageView homescreen;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//instantiate UI items
-		scanBtn = (Button)findViewById(R.id.scan_button);
-		formatTxt = (TextView)findViewById(R.id.scan_format);
-		contentTxt = (TextView)findViewById(R.id.scan_content);
+		// instantiate UI items
+		formatTxt = (TextView) findViewById(R.id.scan_format);
+		contentTxt = (TextView) findViewById(R.id.scan_content);
+		homescreen = (ImageView) findViewById(R.id.homescreen);
 
-		//listen for clicks
-		scanBtn.setOnClickListener(this);
-		
-		
-		
+		homescreen.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent ev) {
+				Log.d("onTouch", "Entered onTouch");
+				final int action = ev.getAction();
+
+				final int evX = (int) ev.getX();
+				final int evY = (int) ev.getY();
+				switch (action) {
+				case MotionEvent.ACTION_DOWN:
+					Log.d("onTouch", "Action Down");
+					break;
+				case MotionEvent.ACTION_UP:
+					Log.d("onTouch", "Action Up");
+					// On the UP, we do the click action.
+					// The hidden image (image_areas) has three different
+					// hotspots on
+					// it.
+					// The colors are red, blue, and yellow.
+					// Use image_areas to determine which region the user
+					// touched.
+					// (2)
+					Log.d("onTouch", "x=" + evX + ", y=" + evY);
+					int touchColor = getHotspotColor(R.id.homescreen_mask, evX, evY);
+					// Compare the touchColor to the expected values.
+					// Switch to a different image, depending on what color was
+					// touched.
+					// Note that we use a Color Tool object to test whether the
+					// observed color is close enough to the real color to
+					// count as a match. We do this because colors on the screen
+					// do
+					// not match the map exactly because of scaling and
+					// varying pixel density.
+					int tolerance = 1000;
+					// (3)
+					Log.d("onTouch", "Color = " + touchColor + ", Expected Color = " + Color.RED);
+					if (closeMatch(-1237980, touchColor, tolerance)) {
+						Log.d("onTouch", "Changing intent to ProductInfo");
+						startScan();
+					} else if (closeMatch(-14503604, touchColor, tolerance)) {
+						Log.d("onTouch", "Need to change Intent to login!!!!");
+					}
+					break;
+				} // end switch
+
+				Log.d("onTouch", "Done");
+				return true;
+			}
+		});
+
+		// listen for clicks
+		Log.d("MainActivity", "Done with init");
+
 	}
-
-	public void onClick(View v){
-		//check for scan button
-		if(v.getId()==R.id.scan_button){
-			//instantiate ZXing integration class
-			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-			//start scanning
-			scanIntegrator.initiateScan();
-		}
+	
+	public void switchToProductInfo() {
+		Intent productInfoIntent = new Intent(this, ProductInfoActivity.class);
+		startActivity(productInfoIntent);
+	}
+	
+	public void switchToLogin() {
+		Intent loginActivity = new Intent(this, LoginActivity.class);
+		startActivity(loginActivity);
+	}
+	
+	public void startScan() {
+		//instantiate ZXing integration class
+		//IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+		//start scanning
+		//scanIntegrator.initiateScan();
+		switchToProductInfo();
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		//retrieve result of scanning - instantiate ZXing object
-		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-		//check we have a valid result
+		// retrieve result of scanning - instantiate ZXing object
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(
+				requestCode, resultCode, intent);
+		// check we have a valid result
 		if (scanningResult != null) {
-			//get content from Intent Result
+			// get content from Intent Result
 			String scanContent = scanningResult.getContents();
-			//get format name of data scanned
+			// get format name of data scanned
 			String scanFormat = scanningResult.getFormatName();
-			//output to UI
-			formatTxt.setText("FORMAT: "+scanFormat);
-			contentTxt.setText("CONTENT: "+scanContent);
-			
-			Intent productInfoIntent = new Intent(this, ProductInfoActivity.class);
-		    startActivity(productInfoIntent);
-		}
-		else{
-			//invalid scan data or scan canceled
-			Toast toast = Toast.makeText(getApplicationContext(), 
+			// output to UI
+			formatTxt.setText("FORMAT: " + scanFormat);
+			contentTxt.setText("CONTENT: " + scanContent);
+
+			Intent productInfoIntent = new Intent(this,
+					ProductInfoActivity.class);
+			startActivity(productInfoIntent);
+		} else {
+			// invalid scan data or scan canceled
+			Toast toast = Toast.makeText(getApplicationContext(),
 					"No scan data received!", Toast.LENGTH_SHORT);
 			toast.show();
 		}
 	}
 
+	public int getHotspotColor(int hotspotId, int x, int y) {
+		ImageView img = (ImageView) findViewById(hotspotId);
+		img.setDrawingCacheEnabled(true);
+		Bitmap hotspots = Bitmap.createBitmap(img.getDrawingCache());
+		img.setDrawingCacheEnabled(false);
+		return hotspots.getPixel(x, y);
+	}
+
+	public boolean closeMatch(int color1, int color2, int tolerance) {
+		if ((int) Math.abs(color1 - color2) > tolerance)
+			return false;
+		return true;
+	} // end match
 }
--- src/com/example/barcodescanningapp/ProductInfoActivity.java
+++ src/com/example/barcodescanningapp/ProductInfoActivity.java
@@ -1,46 +1,32 @@
 package com.example.barcodescanningapp;
 
-<<<<<<< HEAD
-=======
-import com.example.barcodescanningapp.cosmetic.ProductMockData;
-import com.google.zxing.integration.android.IntentIntegrator;
-
->>>>>>> 4ad99f12a2d7c6330148d1f83848844fbe5f36a9
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-<<<<<<< HEAD
 import android.view.View.OnClickListener;
 import android.widget.Button;
-=======
-import android.widget.TextView;
-
-public class ProductInfoActivity extends Activity {
->>>>>>> 4ad99f12a2d7c6330148d1f83848844fbe5f36a9
+import android.widget.ImageView;
 
 public class ProductInfoActivity extends Activity implements OnClickListener {
-	private Button allerBtn;
+
+	private Button alergyButton;
+	private ImageView productChemicals;
+	private boolean changeImage = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d("onCreate", "starting up product info!");
 		setContentView(R.layout.activity_product_info);
-		allerBtn = (Button)findViewById(R.id.alergies);
-		allerBtn.setOnClickListener(this);
-		// TextView lView = (TextView)findViewById(R.id.productName);
-		// lView.setText(ProductMockData.getProduct1().getName());
-	}
 
-<<<<<<< HEAD
-	public void onClick(View v) {
-		// check for scan button
-		System.out.println("SSSSS");
-		if (v.getId() == R.id.alergies) {
-			
-			Intent productInfoIntent = new Intent(this, Allergic.class);
-		    startActivity(productInfoIntent);
-=======
+		alergyButton = (Button) findViewById(R.id.alergies);
+		productChemicals = (ImageView) findViewById(R.id.product_chemicals);
+
+		alergyButton.setOnClickListener(this);
+
 		// TextView lView = (TextView)findViewById(R.id.productName);
 		// lView.setText(ProductMockData.getProduct1().getName());
 	}
@@ -48,11 +34,8 @@ public class ProductInfoActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		// check for scan button
 		if (v.getId() == R.id.alergies) {
-			// instantiate ZXing integration class
-			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-			// start scanning
-			scanIntegrator.initiateScan();
->>>>>>> 4ad99f12a2d7c6330148d1f83848844fbe5f36a9
+			Intent allergies = new Intent(this, Allergic.class);
+			startActivity(allergies);
 		}
 	}
 
@@ -63,4 +46,24 @@ public class ProductInfoActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d("activityResult", "Checking result of last activity");
+		if (resultCode == RESULT_OK) {
+			super.onActivityResult(requestCode, resultCode, data);
+			int newImage = data.getIntExtra("newImage",	R.drawable.productchemicals);
+			Log.d("activityResult", "Activity result was: " + newImage);
+			productChemicals.setImageResource(newImage);
+		}
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d("onStart", "Entering onStart");
+		if (changeImage)
+			productChemicals.setImageResource(R.drawable.selectallergies_bottom);
+		else
+			productChemicals.setImageResource(R.drawable.productchemicals);
+	}
 }
