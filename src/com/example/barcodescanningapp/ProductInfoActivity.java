4ad99f12a2d7c6330148d1f83848844fbe5f36a9
mightymouse07 <carlos.cook07@gmail.com>
2014-01-25 21:01:28 -0800

    new images and stuff

--- src/com/example/barcodescanningapp/ProductInfoActivity.java
+++ src/com/example/barcodescanningapp/ProductInfoActivity.java
@@ -1,10 +1,12 @@
 package com.example.barcodescanningapp;
 
 import com.example.barcodescanningapp.cosmetic.ProductMockData;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class ProductInfoActivity extends Activity {
@@ -14,8 +16,18 @@ public class ProductInfoActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_info);
 
-		//TextView lView = (TextView)findViewById(R.id.productName);
-		//lView.setText(ProductMockData.getProduct1().getName());
+		// TextView lView = (TextView)findViewById(R.id.productName);
+		// lView.setText(ProductMockData.getProduct1().getName());
+	}
+
+	public void onClick(View v) {
+		// check for scan button
+		if (v.getId() == R.id.alergies) {
+			// instantiate ZXing integration class
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			// start scanning
+			scanIntegrator.initiateScan();
+		}
 	}
 
 	@Override
