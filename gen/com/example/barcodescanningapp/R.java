46b9e3b61ce90f2d9bad8069042ad641aff0d351
mightymouse07 <carlos.cook07@gmail.com>
2014-01-25 19:02:41 -0800

    Wired up some real controls and partial mock

--- gen/com/example/barcodescanningapp/R.java
+++ gen/com/example/barcodescanningapp/R.java
@@ -27,17 +27,37 @@ public final class R {
     public static final class drawable {
         public static final int bg01=0x7f020000;
         public static final int card_background=0x7f020001;
-        public static final int ic_action_search=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int search_bg_shadow=0x7f020004;
+        public static final int cart=0x7f020002;
+        public static final int down=0x7f020003;
+        public static final int heart=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int product1=0x7f020007;
+        public static final int productchemicals=0x7f020008;
+        public static final int search_bg_shadow=0x7f020009;
+        public static final int star1=0x7f02000a;
+        public static final int star2=0x7f02000b;
+        public static final int up=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0005;
+        public static final int ImageButton01=0x7f0a0004;
+        public static final int ImageButton02=0x7f0a0006;
+        public static final int ImageButton03=0x7f0a0007;
+        public static final int action_settings=0x7f0a0011;
+        public static final int imageButton1=0x7f0a0005;
+        public static final int imageView1=0x7f0a0008;
+        public static final int imageView2=0x7f0a000f;
         public static final int productName=0x7f0a0003;
         public static final int scan_button=0x7f0a0000;
         public static final int scan_content=0x7f0a0002;
         public static final int scan_format=0x7f0a0001;
-        public static final int txtView=0x7f0a0004;
+        public static final int scrollView1=0x7f0a000e;
+        public static final int star1=0x7f0a000c;
+        public static final int star2=0x7f0a000d;
+        public static final int star3=0x7f0a000a;
+        public static final int star4=0x7f0a0009;
+        public static final int star5=0x7f0a000b;
+        public static final int txtView=0x7f0a0010;
     }
     public static final class integer {
         public static final int config_slide_time=0x7f060000;
